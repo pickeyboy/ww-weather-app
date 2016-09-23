c4c672f04b8eb9377924ee5cc3daec1a6cb6b6cd
Ashwin <ashwinagrawal@hotmail.com>
2016-09-23 11:51:12 +1000

    Data binding image adapter

--- ww-weather-app/app/src/main/java/au/com/challenge/ww/weatherapp/adapter/DataBindingAdapters.java
+++ ww-weather-app/app/src/main/java/au/com/challenge/ww/weatherapp/adapter/DataBindingAdapters.java
@@ -1,11 +1,13 @@
 package au.com.challenge.ww.weatherapp.adapter;
 
 import android.databinding.BindingAdapter;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.widget.ImageView;
 
 public class DataBindingAdapters {
     @BindingAdapter("android:src")
-    public static void setImageResource(ImageView imageView, int resource){
+    public static void setImageResource(@NonNull ImageView imageView, @DrawableRes int resource){
         imageView.setImageResource(resource);
     }
 }
